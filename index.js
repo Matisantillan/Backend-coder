6edf5f1b3768bfc281ddc6af3cbbb7c2dcf6cefe
Matias Santillan <matisantillana@gmail.com>
2023-04-11 10:37:42 -0300

    segundo desafio y tercero incompleto

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
-class ProductManager{
-    constructor(){
-        this.products = []
+import fs from 'fs'
+import express from 'express';
+
+
+
+
+const writeFile = (path, products) => fs.promises.writeFile(path, JSON.stringify({products}));
+
+const readFile = async(path) => {
+    const asyncGetProducts = await fs.promises.readFile(path)
+
+    const turnIntoParse = JSON.parse(asyncGetProducts)
+    return turnIntoParse
+}
+
+
+
+export default class ProductManager{
+    constructor(path){
+        
+        this.products = [];
+        this.path = path;
     };
 
-    getProducts(){
-        return this.products
+    
+
+    initialize = async() => {
+        const existsFile = fs.existsSync(this.path)
+
+        if(existsFile){
+            console.log('El archivo ya existia')
+            const {products} = await readFile(this.path)
+            this.products = products
+            console.log(this.products)
+        }else{
+            await writeFile(this.path, this.products)
+            console.log('El archivo se creo correctamente')
+        }
+    }
+
+    getProducts = async() => {
+        const jsonData = await readFile(this.path)
+        return jsonData
 
     };
 
-    addProduct({title, description, price, thumbnail, code, stock}){
+    addProduct = async ({title, description, price, thumbnail, code, stock}) => {
 
         const findedProduct = this.products.find((product) => product.title === title || product.code === code)
-
+        
         if(findedProduct){
             console.log(`Error, ya existe un producto con ese titulo o codigo: ${title} - ${code}`)
 
@@ -20,11 +56,11 @@ class ProductManager{
             const id = this.products.length + 1;
 
             this.products.push({id, title, description, price, thumbnail, code, stock});
-            
-        };    
-    };
 
-    
+        };
+        await writeFile(this.path, this.products);
+        console.log('Producto agregado correctamente')    
+    };
 
     getProductsById(id){
         const findedProduct = this.products.find((product) => product.id === id)
@@ -36,38 +72,170 @@ class ProductManager{
         };
     };
 
+    updateProduct = async(id, newProduct) => {
+        const findIndexProduct = this.products.findIndex((product) => product.id === id)
+        if(findIndexProduct !== -1){
+            const id = this.products[findIndexProduct].id
+
+            this.products[findIndexProduct] = {
+                id, 
+                ...newProduct
+            }
+            await writeFile(this.path, this.products);
+            console.log('Producto actualizado correctamente')
+
+        }else {
+            console.log('No se encuentra un producto con el id solicitado')
+        }
+
+    }
+
+    deleteProduct = async(id) => {
+        const findIndexProduct = this.products.findIndex((product) => product.id === id)
+        if(findIndexProduct !== -1){
+            const newProducts = this.products.filter(product => product.id !== id)
+
+            await writeFile(this.path, newProducts)
+            console.log('Producto eliminado satisfactoriamente')
+
+        }else {
+            console.log('No se encuentra un producto con el id solicitado')
+        }
+    }
+
 
 };
 
-const productManager = new ProductManager();
+async function main () {
+    const app = express()
+    const productManager = new ProductManager('./productos.json');
+
+    await productManager.initialize();
+
+    let products = await productManager.getProducts();
+    //console.log(products);
+
+    const newProduct = {
+        title: "Camisa a cuadros",
+        description: "Camisa a cuadros, corte italiano",
+        price: 500,
+        thumbnail: "small picture of the product",
+        code: "B305",
+        stock: 35,
+        type:"module"
+    }
+
+    const newProduct2 = {
+        title: "Jogger camuflado",
+        description: "Pantalon jogger camuflado",
+        price: 750,
+        thumbnail:"small picture of the product",
+       code: "C100",
+        stock: 15
+    }
+
+    
+
+
+    const newProduct3 = {
+        title: "Buzo polar",
+        description: "Buzo polar con capucha",
+        price: 1100,
+        thumbnail: "small picture of the product",
+        code: "F30",
+        stock: 33,
+        type:"module"
+
+    }
+    
+    await productManager.addProduct(newProduct)
+    await productManager.addProduct(newProduct2)
+    await productManager.addProduct(newProduct3)
+
+}
+
+    main();
+
+
+
+    
+/*
+    products = await productManager.getProducts();
+    //console.log(products)
+  
+    app.get('/products', (req, res) =>{
+        res.send(products)
+    })
+
+    app.get('/products/:id', (req, res) =>{
+        products.id = req.params
+        res.send(products[products.id])
+    })
+
+    app.listen(3000, () => {
+        console.log('running from express')
+    })
+    
+    
+}
 
 
 
 
-const newProduct1 = {
-    title: 'Camisa a cuadros',
-    description: 'Camisa a cuadros, corte italiano',
-    price: 500,
-    thumbnail:'small picture of the product',
-    code: 'B305',
-    stock: 35,
 
-};
-const newProduct2 = {
-    title: 'Jogger camuflado',
-    description: 'Pantalon jogger camuflado',
-    price: 750,
-    thumbnail:'small picture of the product',
-    code: 'C100',
-    stock: 15,
 
-};
+
+
+
+
+
+
+
+
+productos
+[   
+    {
+        "title": "Camisa a cuadros",
+        "description": "Camisa a cuadros, corte italiano",
+        "price": 500,
+        "thumbnail": "small picture of the product",
+        "code": "B305",
+        "stock": 35,
+        "type":"module"
+
+    },
+    {
+        "title": "Jogger camuflado",
+        "description": "Pantalon jogger camuflado",
+        "price": 750,
+        "thumbnail":"small picture of the product",
+        "code": "C100",
+        "stock": 15
+        
+
+    },
+    {
+        "title": "Medias invisibles",
+        "description": "Medias, tipo soquetes invisibles",
+        "price": 150,
+        "thumbnail":"small picture of the product",
+        "code": "A200",
+        "stock": 15
+        
+
+    }
+]
+
+
 
 productManager.addProduct(newProduct1);
-console.log(productManager.getProducts());
-productManager.addProduct(newProduct1);
-//productManager.addProduct(newProduct2);
+productManager.addProduct(newProduct2);
 
-console.log(productManager.getProductsById(250));
+console.log(productManager.deleteProduct(6))
+console.log(productManager.deleteProduct(2))
 console.log(productManager.getProductsById(1));
-//console.log(productManager.getProductsById(2));
+console.log(productManager.getProductsById(2));
+
+
+
+*/
